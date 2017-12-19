51df8e1af67ea745b6ec1d1edfd1dfe8a49fa32a
zjj <467627364@qq.com>
2017-12-19 12:15:02 +0800

    封装

--- src/mixins/base.js
+++ src/mixins/base.js
@@ -42,4 +42,54 @@ const $post = (url, data = {}, loading = false) => {
   })
 }
 
-export {$post};
+/**
+ * 单个图片上传函数
+ * @param {*} filePath 
+ */
+const $upload = (filePath) => new Promise((resolve, reject) => {
+  wepy.uploadFile({
+    url: 'https://www.us800.cn/plupload/upload.jsp',
+    filePath,
+    name: 'file',
+    success(res) {
+      const {data} = res;
+      const {result} = data;
+      if (result === 'err') {
+        reject(result);
+      }else {
+        resolve(data);
+      }
+    },
+    fail(err) {
+      reject(err);
+    }
+  })
+})
+
+
+const $uploadImg = (count = 1, ) => new Promise((resolve, reject) => {
+  wepy.chooseImage({
+    count,
+    sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+    sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+    success(res) {
+      const tempFilePaths = res.tempFilePaths;
+      const imgUrls = [];
+      if (tempFilePaths instanceof Array) {
+        for(let i = 0; i < tempFilePaths.length; i++) {
+          $upload(tempFilePaths[i])
+          .then(data => {
+            console.log(data)
+            imgUrls.push(data);
+          })
+          .catch(err => {
+            reject(err)
+          })
+        }
+        resolve(imgUrls)
+      }
+    }
+  })
+})
+
+export {$post, $uploadImg};
